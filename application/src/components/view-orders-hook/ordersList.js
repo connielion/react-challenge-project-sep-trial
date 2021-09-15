d6b555e96d39b41254bdff4494cd4a92cd937194
Connie Lai <clai011@ucr.edu>
2021-09-15 14:38:08 -0700

    feat(ordersList): created test case for tzero-padded time format

--- application/src/components/view-orders-hook/ordersList.js
+++ application/src/components/view-orders-hook/ordersList.js
@@ -7,9 +7,14 @@ const OrdersList = (props) => {
             <h2>There are no orders to display</h2>
         </div>
     );
+ 
+    const zeroPad = num =>  num < 10 ? '0'+num : num;
 
     return orders.map(order => {
         const createdDate = new Date(order.createdAt);
+        const hour = createdDate.getHours(), 
+            minutes = createdDate.getMinutes(),
+            seconds = createdDate.getSeconds();
         return (
             <div className="row view-order-container" key={order._id}>
                 <div className="col-md-4 view-order-left-col p-3">
@@ -17,7 +22,7 @@ const OrdersList = (props) => {
                     <p>Ordered by: {order.ordered_by || ''}</p>
                 </div>
                 <div className="col-md-4 d-flex view-order-middle-col">
-                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
+                    <p>Order placed at {`${zeroPad(hour)}:${zeroPad(minutes)}:${zeroPad(seconds)}`}</p>
                     <p>Quantity: {order.quantity}</p>
                 </div>
                 <div className="col-md-4 view-order-right-col">
