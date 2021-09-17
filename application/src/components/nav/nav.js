15fea38d62676f54a314a6d4ab1de92fb33cba8b
Connie Lai <clai011@ucr.edu>
2021-09-17 15:36:20 -0700

    feat(nav): import logoutUser, useDispatch, connect, and useHistory to clear user info and redirect user when logging out

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useDispatch, connect } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import { logoutUser } from '../../redux/actions/authActions';
 import "./nav.css";
 
-const Nav = (props) => {
+const mapStateToProps = state => ({
+    email: state.auth.email
+})
+
+const Nav = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+
+    const logOutOnClick = () => {
+        dispatch(logoutUser());
+        history.push('/');
+    }
     return (
         <div className="nav-strip">
             <Link to={"/order"} className="nav-link">
@@ -15,13 +29,13 @@ const Nav = (props) => {
                     <label className="nav-label">View Orders</label>
                 </div>
             </Link>
-            <Link to={"/login"} className="nav-link">
+            <div onClick={logOutOnClick} className="nav-link">
                 <div className="nav-link-style">
                     <label className="nav-label">Log Out</label>
                 </div>
-            </Link>
+            </div>
         </div>
     );
 }
 
-export default Nav;
+export default connect(mapStateToProps)(Nav);
