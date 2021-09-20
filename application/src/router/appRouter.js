1ce81ebece67e4070a1852f440c6f7ff7ce8858c
Connie Lai <clai011@ucr.edu>
2021-09-20 14:31:03 -0700

    feat(router): Install react-router-guards, replace Route with GuardedRoutes and wrapped them with GuardProvider in appRouter.js; create NotFound for invalid routes

--- application/src/router/appRouter.js
+++ application/src/router/appRouter.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { Main, Login, OrderFormHook, ViewOrdersHook } from '../components';
+import NotFound from '../components/NotFound/NotFound'
+import { GuardProvider, GuardedRoute} from 'react-router-guards';
+import guardsList from './guards/guards';
+import { useSelector } from 'react-redux';
 
-const AppRouter = (props) => {
+const AppRouter = () => {
+  const token = useSelector(state => state.auth.token);
+
   return (
     <Router>
-      <Route path="/" exact component={Main} />
-      <Route path="/login" exact component={Login} />
-      <Route path="/order" exact component={OrderFormHook} />
-      <Route path="/view-orders" exact component={ViewOrdersHook} />
+      <GuardProvider guards={guardsList}>
+        <GuardedRoute path="/" exact component={Main} />
+        <GuardedRoute path="/login" exact component={Login} />
+        <GuardedRoute path="/order" exact component={OrderFormHook} meta={{ auth: true, token }}/>
+        <GuardedRoute path="/view-orders" exact component={ViewOrdersHook} meta={{ auth: true, token}}/>
+        <GuardedRoute path="*" component={NotFound} />
+      </GuardProvider>
     </Router>
   );
 }
